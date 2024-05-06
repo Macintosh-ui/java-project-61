b7d955f17f377ff1fc37417b56cebd9d3702957e
Pavlov Denis <pda33@icloud.com>
2024-05-06 16:03:45 +0300

    refactored progression

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -12,36 +12,49 @@ public class Progression {
         int correctAnswers = 0;
 
         while (correctAnswers != 3){
-            int[] numbers = new int[random.nextInt(5, 10)];
-            int coefficient = random.nextInt(1,10);
-            numbers[0] = random.nextInt(20);
-            for (int i = 1; i < numbers.length; i++) {
-                numbers[i] = numbers[0] + i * coefficient;
-            }
-            int bound = numbers.length;
-            int hiddenIndex = random.nextInt(1, bound);
-            String[] stringNumbers = new String[bound];
-            for (int k = 0; k < bound; k++) {
+            int[] numbers = getProgression();
+            String[] stringNumbers = new String[numbers.length];
+            for (int k = 0; k < numbers.length; k++) {
                 stringNumbers[k] = String.valueOf(numbers[k]);
             }
+            int hiddenIndex = random.nextInt(1, numbers.length);
             stringNumbers[hiddenIndex] = "..";
             System.out.println("Question: ");
-            for (int j = 0; j < bound; j++) {
+            for (int j = 0; j < numbers.length; j++) {
                 System.out.print(stringNumbers[j]+ " ");
             }
             System.out.println(" ");
             String userAnswer = answer.nextLine();
             String stringHidden = String.valueOf(numbers[hiddenIndex]);
-            if (userAnswer.equals(stringHidden)) {
+            
+            if (checkAnswer(stringHidden, userAnswer) == true) {
                 System.out.println("Correct!");
                 correctAnswers++;
             }
-            else if (!userAnswer.equals(stringHidden)) {
+            else {
                 System.out.println("'" + userAnswer + "' is wrong answer ;(. Correct answer was '" + stringHidden + "'");
                 System.out.println("Let's try again, " + userName);
                 return;
             }
             }
         System.out.println("Congratulations, " + userName + "!");
+    }
+    private static int[] getProgression() {
+        Random random = new Random();
+        int[] numbers = new int[random.nextInt(5, 10)];
+        int coefficient = random.nextInt(1,10);
+        numbers[0] = random.nextInt(20);
+        for (int i = 1; i < numbers.length; i++) {
+            numbers[i] = numbers[0] + i * coefficient;
+        }
+        return numbers;
+    }
+    private static boolean checkAnswer(String question, String answer) {
+        if (answer.equals(question)) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 }
