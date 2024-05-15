2b871a20843fec6db63c69a08781b519a83c3fde
Pavlov Denis <pda33@icloud.com>
2024-05-15 07:51:27 +0300

    method swap from Engine to App

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -18,7 +18,7 @@ public class App {
     private static final  int EXIT = 0;
     private static final Scanner NUMBER_CHOOSE = new Scanner(System.in);
     public static void main(String[] args) {
-        Engine.printGames();
+        printGames();
         int chosenNumber = App.NUMBER_CHOOSE.nextInt();
         switch (chosenNumber) {
             case GREET:
@@ -43,5 +43,16 @@ public class App {
             default:
                 System.out.println("Invalid number");
         }
+    }
+    public static void printGames() {
+        System.out.println("Welcome to the Brain Games!");
+        System.out.println("Please enter the game number and press Enter");
+        System.out.println("1 - Greet");
+        System.out.println("2 - Even");
+        System.out.println("3 - Calc");
+        System.out.println("4 - GCD");
+        System.out.println("5 - Progression");
+        System.out.println("6 - Prime");
+        System.out.println("0 - Exit");
     }
 }
--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -43,15 +43,5 @@ public static void engineGame(String firstQuestion, String secondQuestion, Strin
             }
         System.out.println("Congratulations, " + userName + "!");
     }
-    public static void printGames() {
-        System.out.println("Welcome to the Brain Games!");
-        System.out.println("Please enter the game number and press Enter");
-        System.out.println("1 - Greet");
-        System.out.println("2 - Even");
-        System.out.println("3 - Calc");
-        System.out.println("4 - GCD");
-        System.out.println("5 - Progression");
-        System.out.println("6 - Prime");
-        System.out.println("0 - Exit");
-    }
+
 }
